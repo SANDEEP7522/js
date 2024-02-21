a63740381f3d5b693593edb295b5468aef5190d9
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-21 12:16:01 +0000

    variavles

---
+++ Basics1/data-type.js
@@ -0,0 +1,58 @@
+// premitive (Stack)
+/*
+Null 
+Number
+String
+Symbol
+BigInt
+Boolean
+Undefined
+*/
+const firstName = "Sandeep"
+const age = 20
+const score = 200
+let isLoggenId = true
+const nodeVersion = null
+let bankBalance = undefined
+const buttonId = symbol("id")
+const aReallyLongNumber = 99225684215n 
+
+// Reference (non-primitive) (Heap)
+
+/*
+ Array
+ Objects
+ Function
+*/
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8,]
+const heroes = [
+    "superman",
+     "batman",
+      "flash",
+      true,
+      123,
+    ]
+
+
+const userObject = {
+    name: "Sandeep",
+    age: 22,
+    isLoggedId: true
+}
+
+
+//Function
+const sayHellow = function(){
+    console.log("Sandeep")
+}
+const regiterAUser = function(){}
+const loginUser = function(){}
+
+
+
+
+
+
+
+
