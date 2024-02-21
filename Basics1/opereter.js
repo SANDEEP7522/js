ad50d6fbfbb252d9416494a8d950f8313b04468e
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-21 13:03:22 +0000

    operator

---
+++ Basics1/opereter.js
@@ -0,0 +1,28 @@
+// Arithmetic operaters
+// 5+5
+// 6-5
+// 5*5
+// 5/5
+// 2**3 => 2*2*2
+// 10%3
+
+
+const numberOne = 5
+const numberTwo = 5
+const numberThree = numberOne + numberTwo
+console.log(numberThree)
+
+// Assignment Operators
+// let valueTow = 3
+// valueTwo += 10
+
+// Comparision Operator
+
+let pw = 3
+let pwskills = "3"  //check also data types
+console.log(pw === pwskills)
+
+let pws = 3
+let pwskill = 4
+console.log(pws != pwskill)
+
