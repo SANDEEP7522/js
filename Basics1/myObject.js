a139327172942387b4b0647541ab0c0cae8740a6
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-22 13:11:30 +0000

    Object

---
+++ Basics1/myObject.js
@@ -0,0 +1,24 @@
+const user = {
+    username: "SS",
+    name: "Sandeep",
+   email: "sa@gmail.cnm",
+    age: 20,
+    PhNO: 222223334,
+    gf: false,
+    bf: true,
+    Address: {
+    city: "Prayagraj",
+    state: "UtterPradesh"
+    }
+}
+
+console.log(user.age)
+console.log(user["age"])
+console.log(user['age'])
+console.log(user.Address.state)
+//console.log(user["address"]["state"])
+
+
+
+
+
