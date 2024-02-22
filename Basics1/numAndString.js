5d45499c218183971e3687d2629cd10b605a220b
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-22 07:56:39 +0000

    number

--- Basics1/numAndString.js
+++ Basics1/numAndString.js
@@ -1,4 +1,5 @@
- const firstName = "Sandeep"
+/*
+const firstName = "Sandeep"
  const lastName = 'Sahu'
  console.log(`my name is ${firstName} and last name is ${lastName}`)
  
@@ -14,9 +15,18 @@ let emailOne = "sahu@gmail.com"
 console.log(emailOne.replace( 'sahu' , 'Sandeep'))
 console.log(email.toUpperCase())
 console.log(email.toUpperCase())
+*/
 
+// Number
 
+const num = new Number(5.234322)
+console.log(num.toFixed(2));
 
+const num1 = 1200000
+console.log(num1.toLocaleString())
+console.log(Number.MAX_VALUE)
+
+console.log(Math);
 
 
 
