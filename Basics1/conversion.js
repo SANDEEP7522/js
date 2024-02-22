c4af070cfa48e315a4c2dea39e346a49bec8f1fe
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-22 07:23:59 +0000

    strings

---
+++ Basics1/conversion.js
@@ -0,0 +1,23 @@
+//let bankBalance = '100'
+//let bankBalance = ''  // emty string print false
+let bankBalance = 'asdf'  // NaN not a number
+
+
+
+//let bankBalanceInt = parseInt(bankBalance)
+//let bankBalanceInt = Boolean(bankBalance)
+let bankBalanceInt = Number(bankBalance)
+
+
+
+console.log(bankBalance)
+console.log(bankBalanceInt)
+
+// console.log(typeof bankBalance)
+ console.log(typeof bankBalanceInt)
+
+//  console.log(3 == 3) // true
+//  console.log(NaN == NaN)  // false it is rule js
+
+console.log(3 + undefined)  // NaN undefine no any space in memory  
+console.log( 3 + null)   // 3 null somithing space is just 0
---
+++ Basics1/numAndString.js
@@ -0,0 +1,26 @@
+ const firstName = "Sandeep"
+ const lastName = 'Sahu'
+ console.log(`my name is ${firstName} and last name is ${lastName}`)
+ 
+const username = new String('Sandeep')
+console.log(username[0])
+console.log(username.length)
+console.log(username.charAt(0))
+console.log(username.substring(0, 4))
+
+let email = "Sa@gmail.com"
+console.log(email.trim())
+let emailOne = "sahu@gmail.com"
+console.log(emailOne.replace( 'sahu' , 'Sandeep'))
+console.log(email.toUpperCase())
+console.log(email.toUpperCase())
+
+
+
+
+
+
+
+
+
+
