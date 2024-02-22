705040f9ec1c8937c0cd2f24e52b4923777e5093
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-22 12:41:17 +0000

    MyArray

---
+++ Basics1/myArray.js
@@ -0,0 +1,33 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7]
+
+// const myNewArray = new Array[1, 2, 3, 4, 5, 6]
+// console.log(myArray[1])
+
+/*
+const dcHeros = ["Sandeep", "Ranjeet", "Anupam", "sonu", "monu", "rohan"]
+// console.log(dcHeros.length)
+
+dcHeros.push("Abhishekh")
+console.log(dcHeros);
+ const lastHero = dcHeros.pop()
+ console.log(dcHeros)
+ dcHeros.unshift("Sahu")
+ console.log(dcHeros)
+ console.log(myArray.length)
+ console.log(dcHeros.toString())
+ console.log(dcHeros.join("-"))
+*/
+
+ const dcHeros = ["Sandeep", "Ranjeet", "Anupam", "sonu", "monu", "rohan"]
+
+console.log(dcHeros.slice(1, 3))
+console.log(dcHeros) // no change
+
+console.log(dcHeros.splice(1, 3))
+console.log(dcHeros)  // change
+
+
+
+
+
+
