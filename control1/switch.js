5458c73d90872db327ad9131bbe92d078f13cb2f
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-23 07:08:01 +0000

    switch

---
+++ control1/switch.js
@@ -0,0 +1,60 @@
+// build a rating system for pizzahut
+
+// 1 star - Poor
+// 2 star - emm, ok
+// 3 star - Average
+// 4 star - cool
+// 5 star - great
+// 6 star - awesome
+// 7 star - nailed
+
+// switch(expression){
+//     case value1:
+//         statement1;
+//         statement2;
+//      break;
+
+//      case value2:
+//         statement3;
+//         statement4;
+//      break;
+//     }
+
+let getStartUser ="9"
+let starInNumber = parseInt(getStartUser)
+
+switch(starInNumber){
+        case 1:
+         console.log("poor");
+         break;
+    
+         case 2:
+         console.log("emm ok");
+         break;
+       
+         case 3:
+            console.log("Avarege");
+            break;
+
+         case 4:
+         console.log("cool");
+         break;
+
+         case 5:
+         console.log("great");
+         break;
+
+         case 6:
+         console.log("Awesome");
+         break;
+
+         case 7:
+         console.log("Nailed");
+         break;
+         
+         default:
+         console.log("Out of game ruls");
+         break;
+         
+        }
+
