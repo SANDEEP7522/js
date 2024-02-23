91dc90fc652cf698a00f016e21a31842451ea081
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-02-23 06:22:11 +0000

    if-else, else-if

--- control1/01-if.js
+++ control1/01-if.js
@@ -1,4 +1,4 @@
-
+/*
 let temperature = 16
   if (temperature < 20){
     console.log("it is really cold")
@@ -24,3 +24,62 @@ let temperature = 16
 
     console.log(`Your score is ${score} and my life score ${life} and 
     my bullets points is ${bullets}`)
+
+    */
+// build a rating system for pizzahut
+
+// 1 star - Poor
+// 2 star - emm, ok
+// 3 star - Average
+// 4 star - cool
+// 5 star - great
+// 6 star - awesome
+// 7 star - nailed
+
+let myStartNumber = "8"
+let myNumber = parseInt(myStartNumber);
+
+if(myNumber == "1")
+{
+    console.log("Poor")
+}
+ else if(myNumber == "2")
+{
+    console.log("emm ok")
+}
+else if(myNumber == "3")
+{
+    console.log("Average")
+}
+else if(myNumber == "4")
+{
+    console.log("Cool")
+}
+else if(myNumber == "5")
+{
+    console.log("Great")
+}
+else if(myNumber == "6")
+{
+    console.log("awesome")
+}
+else if(myNumber == "")
+{
+    console.log("nailed")
+}
+else{
+    console.log("Out of game rulls")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
